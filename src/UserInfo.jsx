3b6524dc9375eb414590daaf8bdfa294e7784c69
minechoo <choominhae@gmail.com>
2023-07-27 11:44:20 +0900

    객체 추가 옵션

--- src/UserInfo.jsx
+++ src/UserInfo.jsx
@@ -2,13 +2,19 @@ import { useQuery } from '@tanstack/react-query';
 
 function UserInfo() {
 	const fectchUser = async ({ queryKey }) => {
+		console.log(queryKey[1]);
 		const response = await fetch(`https://jsonplaceholder.typicode.com/users/${queryKey[1]}`);
 		return await response.json();
 	};
-	const { data, isLoading, isSuccess, isError } = useQuery(['user', 2], fectchUser);
-	console.log(data);
-	console.log(isLoading);
-	console.log(isError);
+	const { data, isLoading, isSuccess, isError } = useQuery(['user', 2], fectchUser, {
+		refetchOnWindowFocus: false,
+		refetchOnMount: false,
+		staleTime: 1000 * 5, //디폴트 : 0초
+		cacheTime: 1000 * 5, //캐쉬데이터 유지시키는 시간 디폴트 5분 해당 시간 뒤에 GC에 의해 메모리에서 값 제거
+	});
+	console.log('data', data);
+	console.log('isLoading', isLoading);
+	console.log('isError', isError);
 	return (
 		<div>
 			<h1>UserInfo</h1>
